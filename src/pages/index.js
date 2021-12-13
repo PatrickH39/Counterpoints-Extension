867a90e5b4e669cd9de2a02126d0d36019540af2
Patrick Huynh <patrick@patrickh.ca>
2021-12-12 17:09:54 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/information">
+            to="/about/general-information">
             Read More 📚
           </Link>
         </div>
