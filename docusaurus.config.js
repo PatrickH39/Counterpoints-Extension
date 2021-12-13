4bb938cf11a8854dd67f8d8d8dcb1beec73dfe74
Patrick Huynh <patrick@patrickh.ca>
2021-12-12 17:04:41 -0800

    Restructure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
             },
             items: [{
                     type: 'doc',
-                    docId: 'information/general-information',
+                    docId: 'about/general-information',
                     position: 'left',
                     label: 'About',
                 },
